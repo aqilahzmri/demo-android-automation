1bcce6afd46d0eb28f7ca53e230b2ec9969957ee
aqilahmsrh <aqilah@regovtech.com>
2024-03-20 12:45:40 +0800

    update

---
+++ app/src/test/java/com/testing/firetasmiktiga/HomeActivityTest.java
@@ -0,0 +1,17 @@
+package com.testing.firetasmiktiga;
+
+//import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class HomeActivityTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
